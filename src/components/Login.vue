9e810087a4bf39aad2156053250a88ab6678c21e
jingjinduo <jingjinduo@126.com>
2021-01-03 09:48:16 +0800

    登录页面

---
+++ src/components/Login.vue
@@ -0,0 +1,124 @@
+<template>
+    <div class="login_container">
+        <div class="login_box">
+            <div class="avatar_box">
+                <img src="../assets/logo.png" alt="">
+            </div>
+            <el-form :model="loginForm" 
+            ref="loginFormRef"
+            :rules="loginFormRules"
+            label-width="0px" class="login_form" >
+                <el-form-item prop="username">
+                    <el-input
+                    prefix-icon="el-icon-search"
+                    v-model="loginForm.username">
+                    </el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input
+                    prefix-icon="el-icon-search"
+                    v-model="loginForm.password"
+                    type="password">
+
+                    </el-input>
+                </el-form-item>
+                <el-form-item class="btns">
+                    <el-button type="primary" @click="login">登录</el-button>
+                    <el-button type="info" @click="resetLoginForm">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Login',
+    components: {  },
+    directives: {  },
+    data() {
+        return {
+            loginForm: {
+                username: 'abc',
+                password: '123'
+            },
+            loginFormRules: {
+                username:[
+                    { required: true, message: '请输入登录名称', trigger: 'blur' },
+                    { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+                ],
+                password:[
+                    { required: true, message: '请输入登录密码', trigger: 'blur' },
+                    { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+                ]
+            }
+        };
+    },
+    mounted() {
+        
+    },
+    methods: {
+        resetLoginForm(){
+            console.log(this);
+            this.$refs.loginFormRef.resetFields();
+        },
+        login() {
+            this.$refs.loginFormRef.validate(async valid =>{
+                console.log(valid);
+                if (!valid) return;
+                // this.$http.post('clock/all',this.loginForm);
+                const {data: result} = await this.$http.get('clock/all');
+                // const result = await this.$http.get('clock/all');
+                if(result.respCode !=='402') return this.$message.error('失败');
+                this.$message.success('成功');
+                this.$router.push("/home");
+            });
+        }
+    },
+};
+</script>
+<style lang="less" scoped>
+.login_container{
+    background-color: #2b4b6b;
+    height: 100%;
+}
+.login_box{
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+
+    .avatar_box{
+        height: 130px;
+        width: 130px;
+        background-color: #fff;
+        border: 1px solid #eee;
+        border-radius: 50%;
+        padding: 10px;
+        box-shadow: 0 0 10px #ddd;
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        img {
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background-color: #eee;
+        }
+    }
+}
+.btns {
+    display: flex;
+    justify-content: flex-end;
+}
+.login_form{
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+</style>
